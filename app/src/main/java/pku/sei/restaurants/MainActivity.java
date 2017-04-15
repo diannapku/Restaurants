c6dc698a5de1620ccd0a37efe22fd8cd1f898432
zhengshuyu1996 <zhengshuyu1996@163.com>
2017-04-15 10:20:37 +0800

    add yanyun.jar

--- app/src/main/java/pku/sei/restaurants/MainActivity.java
+++ app/src/main/java/pku/sei/restaurants/MainActivity.java
@@ -2,6 +2,7 @@ package pku.sei.restaurants;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.yancloud.android.reflection.get.YanCloudGet;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //String test;
-
+        String ip = "10.0.92.249";
+        int maxPort = 1780;
+        String appPakageName = "com.sankuai.meituan.takeoutnew";
+        YanCloudGet api = YanCloudGet.fromPackageName(ip, maxPort, appPakageName);
+        String result = api.get("comm", "getMyOrders", "{\"pageNum\": \"0\"}");
+        System.out.println(result);
     }
     // This is a test!!!
 }
