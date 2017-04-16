8d2fb55ebd12725f2178a4a09caf8c02b92d85c3
diannapku <sunxiaohan@pku.edu.cn>
2017-04-16 14:09:38 +0800

    add address

--- app/src/main/java/pku/sei/restaurants/MainActivity.java
+++ app/src/main/java/pku/sei/restaurants/MainActivity.java
@@ -7,8 +7,8 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.yancloud.android.reflection.get.YanCloudGet;
@@ -32,33 +32,37 @@ public class MainActivity extends AppCompatActivity {
 
         /*
           处理地址栏
+          如果报错且影响其他部分，请注释掉
          */
         Location location = getLocation();
-
+        String address;
+        if (location != null) {
+            address = getAddress(location);
+        } else {
+            address = "wrongAddress";
+        }
+        TextView address_text = (TextView) findViewById(R.id.address_box);
+        address_text.setText(address);
 
         /*
           处理搜索栏
+          好像不用处理 = =
          */
-
 
         /*
           处理ListView
          */
         //初始化一个Adapter
         Model model = new Model();
-
         EntryAdapter entryAdapter = new EntryAdapter(this, R.layout.info_card, model.getEntries());
-
         //通过ID获取listView
         ListView listView = (ListView) findViewById(R.id.ListViewId);
         //设置listView的Adapter
         listView.setAdapter(entryAdapter);
-
-
     }
 
     private Location getLocation() {
-        String locationProvider = "";
+        String locationProvider;
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         //获取所有可用的位置提供器
         List<String> providers = locationManager.getProviders(true);
@@ -69,51 +73,51 @@ public class MainActivity extends AppCompatActivity {
             //如果是Network
             locationProvider = LocationManager.NETWORK_PROVIDER;
         } else {
-            Toast.makeText(this, "没有可用的位置提供器", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "获取位置提供器失败", Toast.LENGTH_SHORT).show();
+            return null;
         }
-        Location location;
 
+        Location location;
         try {
             location = locationManager.getLastKnownLocation(locationProvider);
         } catch (SecurityException e) {
-            Toast.makeText(this, "没有可用的位置提供器", Toast.LENGTH_SHORT).show();
-            location = null;
+            Toast.makeText(this, "获取经纬度失败", Toast.LENGTH_SHORT).show();
+            return null;
         }
         return location;
     }
 
-    private String getAddress(Location location) throws IOException {
+    private String getAddress(Location location) {
         Geocoder geocoder = new Geocoder(this);
-        boolean flag = geocoder.isPresent();
+        if (!Geocoder.isPresent()) {
+            Toast.makeText(this, "geocoder不存在", Toast.LENGTH_SHORT).show();
+            return "wrongAddress";
+        }
         StringBuilder stringBuilder = new StringBuilder();
         try {
             //根据经纬度获取地理位置信息
+            //double latitude，double longitude，int maxResults
             List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
 
             if (addresses.size() > 0) {
                 Address address = addresses.get(0);
-                for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
-                    stringBuilder.append(address.getAddressLine(i)).append("\n");
-                }
-                stringBuilder.append(address.getCountryName()).append("_");//国家
-                stringBuilder.append(address.getFeatureName()).append("_");//周边地址
                 stringBuilder.append(address.getLocality()).append("_");//市
-                stringBuilder.append(address.getPostalCode()).append("_");
-                stringBuilder.append(address.getCountryCode()).append("_");//国家编码
-                stringBuilder.append(address.getAdminArea()).append("_");//省份
-                stringBuilder.append(address.getSubAdminArea()).append("_");
-                stringBuilder.append(address.getThoroughfare()).append("_");//道路
                 stringBuilder.append(address.getSubLocality()).append("_");//香洲区
-                stringBuilder.append(address.getLatitude()).append("_");//经度
-                stringBuilder.append(address.getLongitude());//维度
-                System.out.println(stringBuilder.toString());
+                stringBuilder.append(address.getThoroughfare()).append("_");//道路
+                stringBuilder.append(address.getFeatureName()).append("_");//周边地址
+            } else {
+                Toast.makeText(this, "找不到此地址", Toast.LENGTH_LONG).show();
+                return "wrongAdress";
             }
+
         } catch (IOException e) {
-            Toast.makeText(this, "报错", Toast.LENGTH_LONG).show();
-            e.printStackTrace();
+            Toast.makeText(this, "获取地址失败", Toast.LENGTH_LONG).show();
+            return "wrongAdreess";
         }
         return stringBuilder.toString();
-
     }
+
+
+
 
 }
