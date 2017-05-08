be72be28cfafb94b0e2a34e7937fc3b0c70c0ce1
diannapku <sunxiaohan@pku.edu.cn>
2017-05-08 11:46:09 +0800

    delete photo

--- app/src/main/java/pku/sei/restaurants/MainActivity.java
+++ app/src/main/java/pku/sei/restaurants/MainActivity.java
@@ -25,35 +25,38 @@ public class MainActivity extends AppCompatActivity {
     public LocationClient mLocationClient = null;
     public BDLocationListener myListener = new MyLocationListener();
     private BDLocation location = null;
+    private List<Entry> entries = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        /*
-         activity_main 里面有:
-         地址栏
-         搜索栏
-         一个ListView
-          */
+        // 文字版UI
         setContentView(R.layout.activity_main);
 
-        /*
-          处理地址栏
-         */
+        // 语音版UI
+        //setContentView();
+
+
+        // 获取地址
+
+        // 文字版UI
         TextView address_text = (TextView) findViewById(R.id.address_box);
         address_text.setText("waiting");
 
+        // 语音版UI
+
+
+        // 获取地址开始
         mLocationClient = new LocationClient(getApplicationContext());
         initLocation();
-        //声明LocationClient类
         mLocationClient.registerLocationListener(myListener);
-        //注册监听函数
         mLocationClient.start();
-        //while (location == null);
         //mLocationClient.stop();
-        /*
-          处理搜索栏
-         */
+        // 获取地址结束，地址在location里面。具体获取时间未知，所以使用Location之前需要判断是不是null。
+
+
+        // 搜索栏
+        // 文字版UI
         Button search = (Button)findViewById(R.id.search_btn);
         search.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,32 +67,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // 语音版UI？
+
+
     }
 
     Handler h =  new Handler();
     private void setListView(final String search_str) {
-        /*
-          处理ListView
-         */
+
         String baidu_str = readStream(getResources().openRawResource(R.raw.baidu_result));
         String eleme_str = readStream(getResources().openRawResource(R.raw.eleme_result));
 
-        //初始化一个Adapter
         Log.v("zsy","*********");
         Runnable r = new Runnable(){
             @Override
             public void run() {
                 Model model = new Model();
-                final List<Entry> entries = model.getEntries(search_str, location);
+                entries = model.getEntries(search_str, location);
                 h.post(new Runnable(){
-
                     @Override
                     public void run() {
                         EntryAdapter entryAdapter = new EntryAdapter(MainActivity.this, R.layout.info_card, entries);
-                        //通过ID获取listView
+
+                        // 文字版UI
                         ListView listView = (ListView) findViewById(R.id.ListViewId);
-                        //设置listView的Adapter
                         listView.setAdapter(entryAdapter);
+
+
                     }
                 });
             }
@@ -98,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    // 初始化地址查询方式
     private void initLocation(){
         LocationClientOption option = new LocationClientOption();
         option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
@@ -137,10 +142,10 @@ public class MainActivity extends AppCompatActivity {
         mLocationClient.setLocOption(option);
     }
 
+    // 地址监听器
     public class MyLocationListener implements BDLocationListener {
         @Override
         public void onConnectHotSpotMessage(String s, int i) {
-
         }
 
         @Override
@@ -149,97 +154,7 @@ public class MainActivity extends AppCompatActivity {
             location = tlocation;
             TextView address_text = (TextView) findViewById(R.id.address_box);
             address_text.setText(location.getAddrStr());
-Log.d("sxh", "test");
 
-            //获取定位结果
-//            StringBuffer sb = new StringBuffer(256);
-
-//            sb.append("time : ");
-//            sb.append(location.getTime());    //获取定位时间
-//
-//            sb.append("\nerror code : ");
-//            sb.append(location.getLocType());    //获取类型类型
-
-//            sb.append("\nlatitude : ");
-//            sb.append(location.getLatitude());    //获取纬度信息
-//
-//            sb.append("\nlontitude : ");
-//            sb.append(location.getLongitude());    //获取经度信息
-
-//            sb.append("\nradius : ");
-//            sb.append(location.getRadius());    //获取定位精准度
-
-//            if (location.getLocType() == BDLocation.TypeGpsLocation){
-
-//                // GPS定位结果
-//                sb.append("\nspeed : ");
-//                sb.append(location.getSpeed());    // 单位：公里每小时
-//
-//                sb.append("\nsatellite : ");
-//                sb.append(location.getSatelliteNumber());    //获取卫星数
-//
-//                sb.append("\nheight : ");
-//                sb.append(location.getAltitude());    //获取海拔高度信息，单位米
-//
-//                sb.append("\ndirection : ");
-//                sb.append(location.getDirection());    //获取方向信息，单位度
-
-//                sb.append("addr : ");
-//                sb.append(location.getAddrStr());    //获取地址信息
-
-//                sb.append("\ndescribe : ");
-//                sb.append("gps定位成功");
-
-//            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation){
-
-                // 网络定位结果
-//                sb.append("\naddr : ");
-//                sb.append(location.getAddrStr());    //获取地址信息
-
-//                sb.append("\noperationers : ");
-//                sb.append(location.getOperators());    //获取运营商信息
-//
-//                sb.append("\ndescribe : ");
-//                sb.append("网络定位成功");
-
-//            }
-//            else if (location.getLocType() == BDLocation.TypeOffLineLocation) {
-//
-//                // 离线定位结果
-//                sb.append("\ndescribe : ");
-//                sb.append("离线定位成功，离线定位结果也是有效的");
-//
-//            } else if (location.getLocType() == BDLocation.TypeServerError) {
-//
-//                sb.append("\ndescribe : ");
-//                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
-//
-//            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
-//
-//                sb.append("\ndescribe : ");
-//                sb.append("网络不同导致定位失败，请检查网络是否通畅");
-//
-//            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
-//
-//                sb.append("\ndescribe : ");
-//                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
-//
-//            }
-
-//            sb.append("\nlocationdescribe : ");
-//            sb.append(location.getLocationDescribe());    //位置语义化信息
-
-//            List<Poi> list = location.getPoiList();    // POI数据
-//            if (list != null) {
-//                sb.append("\npoilist size = : ");
-//                sb.append(list.size());
-//                for (Poi p : list) {
-//                    sb.append("\npoi= : ");
-//                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
-//                }
-//            }
-
-//            Log.v("sxh", sb.toString());
         }
     }
 
